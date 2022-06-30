cb3c2abbd6fd405e739bca2dd628effcd6358b33
Kennedy Vinícius <kennedyvnak@gmail.com>
2022-06-30 08:17:56 -0300

    update ranged-fields drawer

--- Assets/Scripts/Editor/GUI/RangedValueAttributeDrawer.cs
+++ Assets/Scripts/Editor/GUI/RangedValueAttributeDrawer.cs
@@ -7,30 +7,36 @@ namespace MetroidvaniaEditor
     [CustomPropertyDrawer(typeof(RangedValueAttribute))]
     public class RangedValueAttributeDrawer : PropertyDrawer
     {
+        // Right/left number fields width
+        private const float k_FieldWidth = 40;
+
+        // Space between the right/left fields and the slider
+        private const float k_FieldSpace = 5;
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             RangedValueAttribute minMaxAttribute = (RangedValueAttribute)attribute;
             string propertyType = property.type;
 
-            label.tooltip = minMaxAttribute.min.ToString("F2") + " to " + minMaxAttribute.max.ToString("F2");
+            label.tooltip = $"{minMaxAttribute.min.ToString("F2")} to {minMaxAttribute.max.ToString("F2")}";
 
             Rect controlRect = EditorGUI.PrefixLabel(position, label);
 
-            Rect[] splitRect = SplitRect(controlRect, 3);
+            SplitRect(controlRect, out Rect leftField, out Rect sliderField, out Rect rightField);
 
             SerializedProperty min = property.FindPropertyRelative("min"), max = property.FindPropertyRelative("max");
 
+            label = EditorGUI.BeginProperty(position, label, property);
             if (propertyType == nameof(RangedFloat))
             {
                 EditorGUI.BeginChangeCheck();
                 float minVal = min.floatValue;
                 float maxVal = max.floatValue;
 
-                //F2 limits the float to two decimal places (0.00).
-                minVal = EditorGUI.FloatField(splitRect[0], float.Parse(minVal.ToString("F2")));
-                maxVal = EditorGUI.FloatField(splitRect[2], float.Parse(maxVal.ToString("F2")));
+                minVal = EditorGUI.FloatField(leftField, minVal);
+                maxVal = EditorGUI.FloatField(rightField, maxVal);
 
-                EditorGUI.MinMaxSlider(splitRect[1], ref minVal, ref maxVal, minMaxAttribute.min, minMaxAttribute.max);
+                EditorGUI.MinMaxSlider(sliderField, ref minVal, ref maxVal, minMaxAttribute.min, minMaxAttribute.max);
 
                 if (minVal < minMaxAttribute.min)
                     minVal = minMaxAttribute.min;
@@ -53,10 +59,10 @@ namespace MetroidvaniaEditor
                 float minVal = min.intValue;
                 float maxVal = max.intValue;
 
-                minVal = EditorGUI.FloatField(splitRect[0], minVal);
-                maxVal = EditorGUI.FloatField(splitRect[2], maxVal);
+                minVal = EditorGUI.FloatField(leftField, minVal);
+                maxVal = EditorGUI.FloatField(rightField, maxVal);
 
-                EditorGUI.MinMaxSlider(splitRect[1], ref minVal, ref maxVal, minMaxAttribute.min, minMaxAttribute.max);
+                EditorGUI.MinMaxSlider(sliderField, ref minVal, ref maxVal, minMaxAttribute.min, minMaxAttribute.max);
 
                 if (minVal < minMaxAttribute.min)
                     maxVal = minMaxAttribute.min;
@@ -70,27 +76,14 @@ namespace MetroidvaniaEditor
                     max.intValue = Mathf.FloorToInt(maxVal);
                 }
             }
+            EditorGUI.EndProperty();
         }
 
-        private static Rect[] SplitRect(Rect rectToSplit, int n)
+        private static void SplitRect(Rect rectToSplit, out Rect left, out Rect slider, out Rect right)
         {
-            Rect[] rects = new Rect[n];
-
-            for (int i = 0; i < n; i++)
-                rects[i] = new Rect(rectToSplit.position.x + (i * rectToSplit.width / n), rectToSplit.position.y, rectToSplit.width / n, rectToSplit.height);
-
-            int padding = (int)rects[0].width - 40;
-            int space = 5;
-
-            rects[0].width -= padding + space;
-            rects[2].width -= padding + space;
-
-            rects[1].x -= padding;
-            rects[1].width += padding * 2;
-
-            rects[2].x += padding + space;
-
-            return rects;
+            left = new Rect(rectToSplit.x, rectToSplit.y, k_FieldWidth, rectToSplit.height);
+            right = new Rect(rectToSplit.x + rectToSplit.width - k_FieldWidth, rectToSplit.y, k_FieldWidth, rectToSplit.height);
+            slider = new Rect(rectToSplit.x + k_FieldSpace + k_FieldWidth, rectToSplit.y, rectToSplit.width - (k_FieldWidth + k_FieldSpace) * 2, rectToSplit.height);
         }
     }
 }
