f1acb6dc807a10b44faf483eeb630c257ccf5543
Kennedy Vinicius <kennedyvnak@gmail.com>
2022-12-11 16:38:38 -0300

    Renamed some files, fix typos and improve settings

--- Assets/Scripts/Systems/Entities/EntityBehaviour.cs
+++ Assets/Scripts/Systems/Entities/EntityBehaviour.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 namespace Metroidvania.Entities {
     /// <summary>Base class for handle all entity behaviours</summary>
     public abstract class EntityBehaviour : MonoBehaviour {
-        public EntityObject entityObject { get; set; }
+        [SerializeField] private EntityObject m_EntityObject;
+        public EntityObject entityObject => m_EntityObject;
     }
 }
